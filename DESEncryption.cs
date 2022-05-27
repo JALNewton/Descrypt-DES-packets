af3dd3899f8e14e629add27e3b1c756c5e7b8e4d
Jose Araujo <47309489+JALNewton@users.noreply.github.com>
2022-05-27 20:54:54 -0300

    Upload de arquivos.

---
+++ DESEncryption.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace DES
+{
+    public static class DESEncryption
+    {
+        private static DESCryptoServiceProvider DESProvider = new DESCryptoServiceProvider()
+        {
+            Mode = CipherMode.CBC,
+            Padding = PaddingMode.None
+        };
+        public static byte[] EncryptData(byte[] data, byte[] iv, byte[] key)
+        {
+            ICryptoTransform encryptor = DESProvider.CreateEncryptor(key, iv);
+            return encryptor.TransformFinalBlock(data, 0, data.Length);
+        }
+
+        public static byte[] DecryptData(byte[] data, byte[] iv, byte[] key)
+        {
+            ICryptoTransform decryptor = DESProvider.CreateDecryptor(key, iv);
+            return decryptor.TransformFinalBlock(data, 0, data.Length);
+        }
+    }
+}
